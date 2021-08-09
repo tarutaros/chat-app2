07910873738b56066e37154969fb17750391f6bb
hoshin <66007415+tarutaros@users.noreply.github.com>
2021-08-09 21:46:34 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+  # get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
   root to: "messages#index"
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
